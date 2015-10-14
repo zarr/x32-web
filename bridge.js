dd2d9837511fea43742c47017086c2595bdf8440
Arto Seppa <zar@iki.fi>
2015-10-14 23:31:19 +0300

    Add logging and periodic xremote call

--- bridge.js
+++ bridge.js
@@ -5,10 +5,15 @@ var oscServer, oscClient;
 
 io.sockets.on('connection', function (socket) {
   socket.on("config", function (obj) {
+    console.log('got config', obj);
     oscServer = new osc.Server(obj.server.port, obj.server.host);
     oscClient = new osc.Client(obj.client.host, obj.client.port);
 
     oscClient.send('/status', socket.sessionId + ' connected');
+
+    setInterval(function () {
+      oscClient.send('/xremote');
+    }, 7000);
 
     oscServer.on('message', function(msg, rinfo) {
       console.log(msg, rinfo);
@@ -17,5 +22,6 @@ io.sockets.on('connection', function (socket) {
   });
   socket.on("message", function (obj) {
     oscClient.send(obj);
+    console.log('from ui', obj);
   });
 });
