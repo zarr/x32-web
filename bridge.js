e4f85d44bd35883a5002bed0728a3babed39ff23
Arto Seppa <zar@iki.fi>
2016-05-12 01:16:01 +0300

    Fix crash on new client, update node-osc, relay oscServer to oscClient
    Crash was caused by trying to rebind to the server socket -- avoided
    by caching the servers for a port. Update to node-osc wasn't strictly
    needed, but was done to prepare for forking it to support relaying
    oscServer to oscClient -- this will allow the bridge to send and
    receive on the same udp socket enabling proper x32 functionality.

--- bridge.js
+++ bridge.js
@@ -1,13 +1,19 @@
 var osc = require('node-osc'),
     io = require('socket.io').listen(8081);
 
-var oscServer, oscClient;
+var oscServers = {};
+var oscClient;
 
 io.sockets.on('connection', function (socket) {
   socket.on("config", function (obj) {
     console.log('got config', obj);
-    oscServer = new osc.Server(obj.server.port, obj.server.host);
-    oscClient = new osc.Client(obj.client.host, obj.client.port);
+    var serverKey = "" + obj.server.port + obj.server.host;
+    var oscServer = oscServers[serverKey];
+    if (typeof oscServer === "undefined") {
+      oscServer = new osc.Server(obj.server.port, obj.server.host);
+      oscServers[serverKey] = oscServer;
+    }
+    oscClient = new osc.Client(obj.client.host, obj.client.port, oscServer);
 
     oscClient.send('/status', socket.sessionId + ' connected');
 
